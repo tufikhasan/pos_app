5273e507228c63b3de928040d541b3f8aeabebf4
Towfik Hasan <tufikhasan05@gmail.com>
2023-07-16 20:27:53 +0600

    JWT Token generate and verify methods created

---
+++ app/Helper/JWT_TOKEN.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Helper;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWT_TOKEN {
+    /**
+     * Generate JWT Token
+     * @param string $user_email
+     * @return string
+     */
+    public static function create_token( $user_email ): string{
+        $key = env( 'JWT_KEY' );
+        $payload = [
+            'iss' => 'pos-token',
+            'iat' => time(),
+            'exp' => time() + ( 60 * 60 * 2 ), // expired after 2 hours
+            'user_email' => $user_email,
+        ];
+        return JWT::encode( $payload, $key, 'HS256' );
+    }
+    /**
+     * Generate JWT Token
+     * @param string $token
+     * @return mixed
+     */
+    public static function verify_token( $token ) {
+        try {
+            $key = env( 'JWT_KEY' );
+            $decode = JWT::decode( $token, new Key( $key, 'HS256' ) );
+            return $decode->user_email;
+        } catch ( \Throwable $th ) {
+            return response()->json( ['status' => 'Invalid', 'message' => 'Unauthorized'], 401 );
+        }
+
+    }
+}
